f6744f29ed971d3c4f299869c01fe2b88b3c5cca
arifizzu <88992071+arifizzu@users.noreply.github.com>
2021-09-24 20:50:42 +0800

    Update form-handler.php

--- form-handler.php
+++ form-handler.php
@@ -4,7 +4,7 @@ $visitor_email = $_POST['email'];
 $subject = $_POST['subject'];
 $message = $_POST['message'];
 
-$email_from = 'info@yourwebsite.com';
+$email_from = 'https://arifizzu.github.io/steins-clothing/contact.html';
 
 $email_subject = 'New Form Submission';
 
